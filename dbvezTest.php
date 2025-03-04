0d2ccfa0936fb92153ecc57c317a11db9a2a5bab
SzedlakBence <72437666484@szily.lan>
2025-03-04 13:03:04 +0100

    a mobilteszt elkészült probara

--- dbvezTest.php
+++ dbvezTest.php
@@ -1,31 +1,53 @@
 <?php
 require_once 'dbvezerlo.php';
+
 class DBcontr_test
 {
-    public function tesztFuttatas()
+    public function tesztFuttat()
     {
-        echo"teszt inditása...\n";
-        $db = new DBController();
-        if($this->csatlakozasteszt($db))
-        {
-            echo "Sikeres csatlakozáás \n";
+        echo "teszt indítása... \n";
+        $db=new DBController();
 
+        if ($this->csatlakozasteszt($db))
+        {
+            echo "Sikeres csati \n";
         }
         else
         {
-            echo "Sikertelen csti \n";
+            echo "SikereTELEN csati \n";
         }
-        $db ->closeDB();
+
+        if($this->SelectQueryteszt($db))
+        {
+            echo "SIKERES Szedlák lekérdezés \n";
+        }
+        else
+        {
+            echo "sikerTELEN Szdelák lekérdezés \n";
+        }
+
+        $db->closeDB();
     }
     private function csatlakozasteszt($db)
     {
-        $informacio=new ReflectionClass($db);
+        $informacio= new ReflectionClass($db);
         $tulajdonsag=$informacio->getProperty('conn');
         $tulajdonsag->setAccessible(true);
         return !is_null($tulajdonsag->getValue($db));
+    }
 
+    private function SelectQueryteszt($db)
+    {
+        //célzott lekérdezés
+        $eredmeny=$db->executeSelectQuery("Select m_type from tbl_mobile
+         where m_id=1");
+        print_r($eredmeny);
+        echo "<br>";
+        return is_array($eredmeny) && !empty($eredmeny) &&
+        isset($eredmeny[0]['m_type'])
+        && $eredmeny[0]['m_type']==1;
     }
 }
 
-$teszt=new DBContr_test();
-$teszt->tesztFuttatas();
+$teszt=new DBcontr_test();
+$teszt->tesztFuttat();
